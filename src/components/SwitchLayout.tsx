6125de748bfbace37c7f4bdc83a1c630c6a655e9
tvjoa <johnscontas@hotmail.com>
2025-12-23 14:49:17 -0300

    Update SwitchLayout.tsx

--- src/components/SwitchLayout.tsx
+++ src/components/SwitchLayout.tsx
@@ -5,7 +5,7 @@ import { Listbox, Transition } from '@headlessui/react'
 
 import useLocalStorage from '../utils/useLocalStorage'
 
-export const layouts: Array<{ id: number; name: 'Grid' | 'List'; icon: IconProp }> = [
+export const layouts: Array<{ id: number; name: 'Grande' | 'Lista'; icon: IconProp }> = [
   { id: 1, name: 'List', icon: 'th-list' },
   { id: 2, name: 'Grid', icon: 'th' },
 ]
