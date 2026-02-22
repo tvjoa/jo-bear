37063b76c21480e5fd1dc57712c3fe432ae2d69b
tvjoa <johnscontas@hotmail.com>
2026-02-21 23:55:52 -0300

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -40,8 +40,7 @@ module.exports = {
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
   protectedRoutes: [
-    '/Biografia/Cabrini (2024) [imdbid-tt14351082]',
-    '/Biografia/Maria Callas (2024) [imdbid-tt22893404]'
+    '/ETS2 Mods',
   ],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
