5cce58b5860407e75a93684e9ea4ef25118f2106
tvjoa <johnscontas@hotmail.com>
2026-07-22 22:31:51 -0300

    Update API endpoints and scope in api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -20,10 +20,12 @@ module.exports = {
   // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
   // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
   authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-  driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
+  // Default document library for https://s8rj.sharepoint.com/sites/grupo ("Shared Documents").
+  // For a different library, use: /sites/{site-id}/drives/{drive-id}.
+  driveApi: 'https://graph.microsoft.com/v1.0/sites/s8rj.sharepoint.com:/sites/grupo:/drive',
 
   // The scope we require are listed here, in most cases you would not need to change this as well.
-  scope: 'user.read files.read.all offline_access',
+  scope: 'user.read files.read.all sites.read.all offline_access',
 
   // Cache-Control header, check Vercel documentation for more details. The default settings imply:
   // - max-age=0: no cache for your browser
